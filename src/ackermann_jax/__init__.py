1fd4a6397c13afe6d1a7dbefce320b2e61c65e54
Lucas Libshutz <lsl94@cornell.edu>
2026-03-19 23:31:13 -0400

    added errorDyn file to separate and make cleaner

--- src/ackermann_jax/__init__.py
+++ src/ackermann_jax/__init__.py
@@ -1,7 +1,7 @@
 from .car import AckermannCarState, AckermannCarInput, AckermannCarModel
 from .car import default_params, default_state, pack_input
-from .car import rotation_error, inject_rotation_error, AckermannCarErrorState
-from .car import zero_error_state, inject_error
-from .car import state_difference, pack_error_state, unpack_error_state, error_dynamics
+from .errorDyn import rotation_error, inject_rotation_error, AckermannCarErrorState
+from .errorDyn import zero_error_state, inject_error
+from .errorDyn import state_difference, pack_error_state, unpack_error_state, error_dynamics
 
 from .parameters import Diagnostics, AckermannGeometry, ContactParams, TireParams, WheelParams, ChassisParams, MotorConfig, AckermannCarParams
