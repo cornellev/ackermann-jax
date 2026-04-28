168d978ead3747ef5582374231f827711c1e71a2
Lucas Libshutz <lsl94@cornell.edu>
2026-04-28 17:00:45 -0400

    here we go boys

--- src/ackermann_jax/__init__.py
+++
@@ -1,8 +0,0 @@
-from .car import AckermannCarState, AckermannCarInput, AckermannCarModel
-from .car import default_params, default_state, pack_input
-from .errorDyn import rotation_error, inject_rotation_error, AckermannCarErrorState
-from .errorDyn import zero_error_state, inject_error
-from .errorDyn import state_difference, pack_error_state, unpack_error_state, error_dynamics
-
-from .parameters import Diagnostics, AckermannGeometry, ContactParams, TireParams, WheelParams, ChassisParams, MotorConfig, AckermannCarParams
-from .ekf import EKFState, ekf_predict, ekf_update, ekf_step, compute_F, compute_H
